f6f6995aa10eea66cb686c751e596cbb49136cf4
Surya Solanki <suryasol@usc.edu>
2019-11-23 13:42:01 -0800

    Create JoinCourse.java

---
+++ src/servlets/JoinCourse.java
@@ -0,0 +1,78 @@
+package servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/JoinCourse")
+public class JoinCourse extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+      
+    public JoinCourse() {
+        super();
+    }
+    
+    /*
+     * This servlet adds an instructor to a course and updates the appropriate tables 
+     */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    HttpSession session = request.getSession(); 
+        String username = (String) session.getAttribute("username");
+        String course = request.getParameter("course");  // TODO - FIX 
+        
+        PreparedStatement ps = null; 
+        Connection conn = null;
+        Statement st = null; 
+        ResultSet rs = null;
+        
+        // TODO - UPDATE WITH FINAL DATABASE
+        String sql = "jdbc:mysql://google/Hmwk4Database?cloudSqlInstance=cs201-lab:us-central1:sql-db-2&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=root&password=111";
+        try {
+            conn = DriverManager.getConnection(sql);
+            st = conn.createStatement(); 
+            
+            // insert instructor and course into instructorCourse table   
+            String statement = "insert into instructorCourse (courseID, instructorID) values ( (select courseID from course where courseName=\'"+ course + "\'), "
+                + "(select instructorID from instructor where username=\'" + username + "\'))"; 
+            ps = conn.prepareStatement(statement);
+            ps.executeUpdate(); 
+            
+        } catch (SQLException sqle) {
+            System.out.println(sqle.getMessage());
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException sqle) {
+                System.out.println(sqle.getMessage());
+            }
+        }
+        
+        // forward to instructor jsp  - TODO FIX FORWARD 
+        // RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/Instructor.jsp");
+        
+        try {
+            dispatch.forward(request, response);
+        } catch(IOException e) {
+            e.printStackTrace();
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } 
+	}
+
+}
