6806c48cd058f7145e9319c1e5a7b8da2588fa61
Flora Sargsyan <fsargsya@usc.edu>
2019-11-22 23:39:00 -0800

    Updated InstructorLoginServlet

--- src/servlets/InstructorLoginServlet.java
+++ src/servlets/InstructorLoginServlet.java
@@ -17,15 +17,23 @@ import javax.servlet.http.HttpSession;
 public class InstructorLoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void service (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		//Get username and password from form
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		//Save in session
-		HttpSession session = request.getSession();
-		session.setAttribute("username", username);
-		session.setAttribute("password", password);
 		
-		RequestDispatcher rd = request.getRequestDispatcher("/Instructor.jsp");
-		rd.forward(request, response);
+		
+		//***************
+		//PLEASE VALIDATE LOGIN AND ACTUALLY LOG IN
+		//use login() and verify()
+		//IF LOGIN SUCCESSFUL SAVE INSTRUCTOR NAME IN SESSION VARIABLE (SEE BELOW)
+			//HttpSession session = request.getSession();
+			//session.setAttribute("first_name", first_name);
+			RequestDispatcher rd = request.getRequestDispatcher("/Instructor.jsp");
+			rd.forward(request, response);
+		//ELSE KEEP ON LOGIN PAGE AND SHOW ERROR MESSAGE (BASICALLY HW3)
+			//RequestDispatcher rd = request.getRequestDispatcher("/InstructorLogin.jsp");
+			//rd.forward(request, response);
 	}
 }
+
