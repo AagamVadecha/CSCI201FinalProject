cccaae930e40726edd90f1284f89a2908b9535fe
Surya Solanki <suryasol@usc.edu>
2019-11-23 16:27:15 -0800

    Added getOfficeHours()

--- src/classes/CourseManager.java
+++ src/classes/CourseManager.java
@@ -129,4 +129,71 @@ public class CourseManager {
         
         return instructorCourses;
     }
+    /* This function returns an ArrayList containing all the information for an instructor's office hours.
+     * The array contains all of the instructor's office hours (for all his/her classes) 
+     * Parameters: instructor's email 
+     * Returns: ArrayList 
+     */
+    public static ArrayList<ArrayList<String>> getOfficeHours(String username){
+        ArrayList<ArrayList<String>> hours = new ArrayList<ArrayList<String>>(); 
+        
+        ArrayList<String> courseNames = new ArrayList<String>();
+        ArrayList<String> day = new ArrayList<String>();
+        ArrayList<String> hourStart = new ArrayList<String>();
+        ArrayList<String> hourEnd = new ArrayList<String>();
+        
+        PreparedStatement ps = null; 
+        Connection conn = null;
+        Statement st = null; 
+        ResultSet rs = null;
+        
+        // TODO - UPDATE WITH FINAL DATABASE
+        String sql = "jdbc:mysql://google/Hmwk4Database?cloudSqlInstance=cs201-lab:us-central1:sql-db-2&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=root&password=111";
+        
+        try {
+            conn = DriverManager.getConnection(sql);
+            st = conn.createStatement(); 
+            
+            String statement = "select courseID from instructorCourse where instructorID=(select instructorID from instructor where username=\'" + username + "\')";
+            
+            ps = conn.prepareStatement(statement);
+            boolean success = ps.execute(statement);
+            
+            if (success) {
+                rs = ps.getResultSet(); 
+                while(rs.next()) {
+                    int courseID = rs.getInt("courseID");
+                    String statement2 = "select courseName, day, hourStart, hourEnd from officeHour o inner join course c on c.courseID=o.courseID and c.courseID=" + courseID;
+                    ps = conn.prepareStatement(statement2);
+                    success = ps.execute(statement2);
+                    
+                    if (success) {
+                        rs = ps.getResultSet(); 
+                        while (rs.next()) {
+                            courseNames.add(rs.getString("courseName"));
+                            day.add(rs.getString("day"));
+                            hourStart.add(Integer.toString(rs.getInt("hourStart")));
+                            hourEnd.add(Integer.toString(rs.getInt("hourEnd")));
+                        }
+                    }
+                }
+            }
+            
+        } catch (SQLException sqle) {
+            System.out.println(sqle.getMessage());
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException sqle) {
+                System.out.println(sqle.getMessage());
+            }
+        }
+        
+        return hours; 
+    }
 }
