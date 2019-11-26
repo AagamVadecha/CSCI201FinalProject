ec116db70eced6c6ca70a7568f49fd3e78c4d323
Surya Solanki <suryasol@usc.edu>
2019-11-25 18:38:54 -0800

    Create SetCourseID.java

---
+++ src/servlets/SetCourseID.java
@@ -0,0 +1,92 @@
+package servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SetCourseID
+ */
+@WebServlet("/SetCourseID")
+public class SetCourseID extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public SetCourseID() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	    String courseName = (String) request.getParameter("value");
+	    int courseID = -1; 
+	    PreparedStatement ps = null; 
+        Connection conn = null;
+        Statement st = null; 
+        ResultSet rs = null;
+        
+        HttpSession session = request.getSession(); 
+        
+        // TODO - UPDATE WITH FINAL DATABASE
+        //String sql = "jdbc:mysql://google/Hmwk4Database?cloudSqlInstance=cs201-lab:us-central1:sql-db-2&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=root&password=111";
+        
+        String sql = "jdbc:mysql://google/OHScheduler"
+            + "?cloudSqlInstance=zhoue-csci201l-lab7:us-central1:sql-db-lab7"
+            + "&socketFactory=com.google.cloud.sql.mysql.SocketFactory" + "&useSSL=false"
+            + "&user=zhoue&password=password1234";
+        
+        try {
+            conn = DriverManager.getConnection(sql);
+            st = conn.createStatement(); 
+            
+            // insert instructor and course into instructorCourse table   
+            String statement = "select courseID from course where courseName=\"" + courseName + "\"" ; 
+            ps = conn.prepareStatement(statement);
+            boolean success = ps.execute(statement); 
+            
+            if (success) {
+                rs = ps.getResultSet();
+                rs.next();
+                courseID = rs.getInt("courseID");
+            }
+            
+        } catch (SQLException sqle) {
+            System.out.println(sqle.getMessage());
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException sqle) {
+                System.out.println(sqle.getMessage());
+            }
+        }
+        
+        session.setAttribute("courseID", courseID);
+        session.setAttribute("courseName", courseName);
+        RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/InstructorCalendar.jsp");
+        
+        try {
+            dispatch.forward(request, response);
+        } catch(IOException e) {
+            e.printStackTrace();
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } 
+	}
+
+}
